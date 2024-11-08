258368744569aa98212b683771f662d22ebcc0d1
OCHHQ <enosejecollins@gmail.com>
2024-11-08 14:36:15 +0100

    4-index

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
+"""Basic Flask application with Babel support."""
+
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
+
 
 app = Flask(__name__)
 
 
 class Config:
-    """Configuration for Babel."""
+    """
+    Configuration for Babel.
+
+    Attributes:
+        LANGUAGES (list): Supported languages.
+        BABEL_DEFAULT_LOCALE (str): Default locale.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -17,19 +27,27 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """Determine the best match with our supported languages."""
-    # Check if a 'locale' parameter was passed in the URL
+def get_locale() -> str:
+    """
+    Determine the best match with our supported languages.
+
+    Returns:
+        str: Selected language code.
+    """
     locale = request.args.get('locale')
     if locale and locale in app.config['LANGUAGES']:
         return locale
-    # If not, try to guess the best match based on the accept headers
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
-    """Render the homepage with translated messages."""
+def index() -> str:
+    """
+    Render the homepage with translated messages.
+
+    Returns:
+        str: Rendered HTML template.
+    """
     return render_template('4-index.html')
 
 
