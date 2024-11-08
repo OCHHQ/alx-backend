6ff3b15e6b883adf9f6b2b1ddf865e234b3bd8dd
OCHHQ <enosejecollins@gmail.com>
2024-11-08 14:24:17 +0100

    3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
-"""Basic Flask app with internationalization support"""
+"""Basic Flask app with internationalization support."""
+
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
+
 
 app = Flask(__name__)
 
 
 class Config:
-    """Configuration for Babel."""
+    """
+    Configuration for Babel.
+
+    Attributes:
+        LANGUAGES (list): Supported languages.
+        BABEL_DEFAULT_LOCALE (str): Default locale.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -18,14 +27,24 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """Determine the best match with our supported languages."""
+def get_locale() -> str:
+    """
+    Determine the best match with our supported languages.
+
+    Returns:
+        str: Selected language code.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
-    """Render the homepage with translated messages."""
+def index() -> str:
+    """
+    Render the homepage with translated messages.
+
+    Returns:
+        str: Rendered HTML template.
+    """
     return render_template('3-index.html')
 
 
