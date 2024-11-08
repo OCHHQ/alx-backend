60b50971dac65f55c0b7ab1e6e82cfdce7b87581
OCHHQ <enosejecollins@gmail.com>
2024-11-08 13:32:20 +0100

    update 0-app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Basic Flask application."""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,9 +8,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Render the homepage."""
+    """
+    Index route.
+
+    Returns:
+        str: Rendered HTML template.
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
