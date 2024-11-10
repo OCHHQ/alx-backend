37257d54fe0989b7571c3a6e4374b083b12084a3
Collins Enoseje <enosejecollins@gmail.com>
2024-11-10 14:55:58 +0100

    task 2 create python Lf name 2-lif

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" LIFO Caching Module """
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFO caching system """
+
+    def __init__(self):
+        """ Initialize the class """
+        super().__init__()
+        self.last_key = None
+
+    def put(self, key, item):
+        """ Add an item to the cache using LIFO strategy """
+        if key is None or item is None:
+            return
+
+        # Add the item to the cache
+        self.cache_data[key] = item
+
+        # Check if the cache exceeds the maximum allowed items
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if self.last_key is not None:
+                print(f"DISCARD: {self.last_key}")
+                del self.cache_data[self.last_key]
+
+        # Update the last key
+        self.last_key = key
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        return self.cache_data.get(key, None)
